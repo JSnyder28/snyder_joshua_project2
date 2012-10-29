fa84fc245a34077297c543c06652a9e70b1f0f2f
Joshua Snyder <jsnyder28@fullsail.edu>
2012-10-29 13:28:02 -0500

    Added a functional append child for-loop, and changed the ul style-type to none.

--- js/main.js
+++ js/main.js
@@ -2,20 +2,40 @@
 // VFW 1211
 // Project 2
 
+// Target element by tag name array
 var uoList = document.getElementsByTagName("li");
-	//console.log(uoList[4]);
+		console.log(uoList[4]);
+// END
 
+// Change border when input field is active/inactive
 var addRecipe = document.getElementById("addIt");
 var rcpField = document.getElementById("rcpName");
 
 var changeBorder = function () {
 		rcpField.setAttribute("class", "hasFocus");
-}
+};
+
 var defaultBorder = function () {
 		rcpField.removeAttribute("class", "hasFocus");
-}
+};
 
 rcpField.addEventListener("focus", changeBorder);
 rcpField.addEventListener("blur", defaultBorder);
+// END
 
-//console.log(rcpName);
+// createElement Methods
+var rcpList = document.createElement ("ol");
+var rcpListItems = document.createElement ("li");
+// END
+
+// appendChild Methods
+var parentList = document.getElementById("rcpInfo");
+parentList.appendChild(rcpList);
+	//rcpList.appendChild(rcpListItems);
+for (i=0; i<5; i++) {  // Use for the creation of the stored recipes page, possibly.
+		var rcpListItems = document.createElement ("li");
+		rcpList.appendChild(rcpListItems);
+}
+// END
+
+console.log(parentList);
