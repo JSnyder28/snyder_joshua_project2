66660537040b4f904e34ca162d46a8b2a24830e0
Joshua Snyder <jsnyder28@fullsail.edu>
2012-10-29 20:36:22 -0500

    Added value checking functions, and they are working correctly.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 // Target element by tag name array
 var uoList = document.getElementsByTagName("li");
-		console.log(uoList[4]);
+		//console.log(uoList[4]);
 // END
 
 // Change border when input field is active/inactive
@@ -30,12 +30,37 @@ var rcpListItems = document.createElement ("li");
 
 // appendChild Methods
 var parentList = document.getElementById("rcpInfo");
+var recipeArr = [
+		"Chicken", "Beef", "Pork", "Turkey", "Ham"
+		];
 parentList.appendChild(rcpList);
 	//rcpList.appendChild(rcpListItems);
-for (i=0; i<5; i++) {  // Use for the creation of the stored recipes page, possibly.
-		var rcpListItems = document.createElement ("li");
+for (i=0, j=recipeArr.length; i<j; i++) {  // Use for the creation of the stored recipes page, possibly.
+		var rcpListItems = document.createElement("li");
+		rcpListItems.innerHTML = recipeArr[i];
+		// Setting attributes dynamically.
+		if (i%2 == 0) {
+				rcpListItems.setAttribute("class", "even");
+		}
 		rcpList.appendChild(rcpListItems);
 }
 // END
 
-console.log(parentList);
+// Checking the value
+var date = document.getElementById("dateAdded");
+
+var getValue = function() {
+		//console.log(date.value);
+};
+// END
+
+// Is a checkbox checked or unchecked.
+var checkbox = document.getElementById("go_to_meal");
+//console.log(goToMeal);
+var getValue = function () {
+		if (checkbox.checked) {
+				console.log(checkbox.value);
+		}
+};
+
+addIt.addEventListener("click", getValue);
