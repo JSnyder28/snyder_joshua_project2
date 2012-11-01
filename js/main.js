f0723493acaea2f8375d4ba4b15bb129bf00669d
Joshua Snyder <jsnyder28@fullsail.edu>
2012-11-01 18:48:47 -0500

    Everything seems to be working corectly.

--- js/main.js
+++ js/main.js
@@ -2,85 +2,6 @@
 // VFW 1211
 // Project 2
 
-/* Target element by tag name array
-var uoList = document.getElementsByTagName("li");
-		//console.log(uoList[4]);
-// END
-
-// Change border when input field is active/inactive
-var addRecipe = document.getElementById("addIt");
-var rcpField = document.getElementById("rcpName");
-
-var changeBorder = function () {
-		rcpField.setAttribute("class", "hasFocus");
-};
-
-var defaultBorder = function () {
-		rcpField.removeAttribute("class", "hasFocus");
-};
-
-rcpField.addEventListener("focus", changeBorder);
-rcpField.addEventListener("blur", defaultBorder);
-// END
-
-// createElement Methods
-var rcpList = document.createElement ("ol");
-var rcpListItems = document.createElement ("li");
-// END */
-
-/* appendChild Methods
-var parentList = document.getElementById("rcpInfo");
-var recipeArr = [
-		"Chicken", "Beef", "Pork", "Turkey", "Ham"
-		];
-parentList.appendChild(rcpList);
-	//rcpList.appendChild(rcpListItems);
-for (i=0, j=recipeArr.length; i<j; i++) {  // Use for the creation of the stored recipes page, possibly.
-		var rcpListItems = document.createElement("li");
-		rcpListItems.innerHTML = recipeArr[i];
-		// Setting attributes dynamically.
-		if (i%2 == 0) {
-				rcpListItems.setAttribute("class", "even");
-		}
-		rcpList.appendChild(rcpListItems);
-}
-// END */
-
-/* Checking the value
-var getCatValue = function () {
-		var _category = document.getElementById("category");
-		//console.log(_category.value);
-};
-
-var getDateValue = function() {
-		var date = document.getElementById("dateAdded");
-		//console.log(date.value);
-};
-
-var getNameValue = function() {
-		var name = document.getElementById("rcpName");
-		//console.log(name.value);
-};
-
-var getDirectionsValue = function () {
-		var _directions = document.getElementById("directions");
-		//console.log(_directions.value);
-};
-
-var getRatingValue = function () {
-		var _rating = document.getElementById("rating");
-		//console.log(_rating.value);
-};
-// END
-
-// Is a checkbox checked or unchecked.
-var getCkValue = function () {
-		var checkbox = document.getElementById("goToMeal");
-		if (checkbox.checked) {
-				//console.log(checkbox.value);
-		}
-};
-// END */
 // Wait until the DOM is ready.
 window.addEventListener("DOMContentLoaded", function() {
 
@@ -104,7 +25,7 @@ window.addEventListener("DOMContentLoaded", function() {
 							makeSelect.appendChild(makeOption);
 					}
 					selectLi.appendChild(makeSelect);
-			}
+			};
 
 			// Find value of checkbox.
 			var getCheckboxValue = function () {
@@ -113,11 +34,6 @@ window.addEventListener("DOMContentLoaded", function() {
 					} else {
 							favoriteValue = "No";
 					}
-			};
-
-			// Find value of range slider.
-			var  getRatingValue = function () {
-						var ratingValue = $('rating').value;
 			};
 
 			// Toggle form visibility on/off.
@@ -146,22 +62,26 @@ window.addEventListener("DOMContentLoaded", function() {
 					// Gather all form values and store it as an object.
 					// Object properties contain an array with form labels and inout values.
 					getCheckboxValue();
-					var item 						= {};
+							var item 						= {};
 							item.cat 				= ["Category:", $('category').value];
 							item.date 			= ["Date Added", $('dateAdded').value];
 							item.rcpName 		= ["Recipe Name:", $('rcpName').value];
 							item.directions = ["Directions:", $('directions').value];
 							item.favorite 	= ["Go-To-Meal?:", favoriteValue];
-							//item.rating 		= ["Rating:", ratingValue];
+							item.rating 		= ["Rating:", $('rating').value];
 				
 					// Save data to local storage using stringify.
 					localStorage.setItem(id, JSON.stringify(item));
 					alert("Recipe added!");
+					window.location.reload();
 
 			};
 
 			var getData = function () {
 					toggleControls("on");
+					if(localStorage.length === 0) {
+							alert("No recipes to view");
+					}
 					// Write data from the local storage to the browser.
 					var makeDiv = document.createElement('div');
 					makeDiv.setAttribute("id", "items");
@@ -189,6 +109,17 @@ window.addEventListener("DOMContentLoaded", function() {
 
 			};
 
+			var clearData = function () {
+					if(localStorage.length === 0) {
+							alert("You have no saved recipes!");
+					} else {
+						localStorage.clear();
+						alert("All recipes have been removed");
+						window.location.reload();
+						return false;
+					}
+			};
+
 			// Variable Defaults
 			var foodCategories = ["--Choose One--", "American", "Chinese", "Italian", "Japanese", "Mexican", "Seasonal"];
 					favoriteValue = "No";
@@ -197,66 +128,8 @@ window.addEventListener("DOMContentLoaded", function() {
 			// Set Link $ Submit Click Events
 			var viewLink = $('viewLink');
 			viewLink.addEventListener("click", getData);
-			//var clearLink = $('clearAll');
-			//clearLink.addEventListener("click", clearData);
+			var clearLink = $('clearAll');
+			clearLink.addEventListener("click", clearData);
 			var save = $('addIt');
 			save.addEventListener("click", storeData);
 });
-
-/* Confirming the creation of a recipe.
-var confirmRcp = function () {
-		var _confirm = confirm("Create Recipe?");
-		if (_confirm) {
-				alert("Recipe was added");
-
-		} else {
-			alert("Recipe was not added");
-		}
-};
-// END
-
-var _cat = document.getElementById("category");
-var _date = document.getElementById("dateAdded");
-var _name = document.getElementById("rcpName");
-var _directions = document.getElementById("directions");
-var _goTo = document.getElementById("goToMeal");
-var _rating = document.getElementById("rating");
-
-// Storing data using local storage.
-var captureLocalData = function () {
-		localStorage.setItem("Category", _cat.value);
-		localStorage.setItem("Date Added", _date.value);
-		localStorage.setItem("Recipe Name", _name.value);
-		localStorage.setItem("Recipe Directions", _directions.value);
-		localStorage.setItem("Go-To-Meal", _goTo.value);
-		localStorage.setItem("Rating", _rating.value);
-};
-
-// END
-
-// Displaying stored data.
-var getLocalData = function () {
-		for(i=0, j=localStorage.length; i<j; i++) {
-				var localKey = localStorage.key(i);
-				var localVal = localStorage.getItem(localKey);
-				console.log(localKey+ " : " +localVal);
-		}
-};
-
-//END
-
-var clearRcps = function () {
-		localStorage.clear();
-};
-
-// EventListeners
-_cat.addEventListener("blur", captureLocalData);
-//_date.addEventListener("blur", captureLocalData);
-//_name.addEventListener("blur", captureLocalData);
-//_directions.addEventListener("blur", captureLocalData);
-//_goTo.addEventListener("blur", captureLocalData);
-//_rating.addEventListener("blur", captureLocalData);
-addIt.addEventListener("click", getLocalData);
-clearAll.addEventListener("click", clearRcps);
-
-// END */
